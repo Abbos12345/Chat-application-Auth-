e8708a4820a22c307ba4609b78e4fb82d948f1e8
Abbos12345 <hodjaevabbos@gmail.com>
2023-05-30 11:40:29 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelContainer, ChannelListContainer, Auth } from './components';
+
+import './App.css';
+
+const cookies = new Cookies();
+
+const apiKey = '';
+const authToken = cookies.get("token");
+
+const client = StreamChat.getInstance(apiKey);  
+
+
+if(authToken){
+  client.connectUser({
+    id: cookies.get('userId'),
+    name: cookies.get('username'),
+    image: cookies.get('avatarURL'),
+    fullName: cookies.get('fullName'),
+    phoneNumber: cookies.get('phoneNumber'),
+    hashedPassword: cookies.get('hashedPassword'),
+  }, authToken)
+}
+
+
+const App = () => {
+
+  if(!authToken) return <Auth />
+
+  return (
+    <div className='app__wrapper'>
+        <Chat client={client} theme="team light">
+            <ChannelListContainer 
+
+            />
+
+            <ChannelContainer 
+            
+            />
+        </Chat>
+    </div>
+  )
+}
+
+export default App
